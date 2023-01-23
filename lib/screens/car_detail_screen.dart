390d7e11afa788d76c1f19fe226b7773d3b93ddd
tkayli10 <kayl8.tan@gmail.com>
2023-01-23 15:46:01 +0800

    testing

--- lib/screens/car_detail_screen.dart
+++ lib/screens/car_detail_screen.dart
@@ -49,11 +49,8 @@ class _CarDetailScreenState extends State<CarDetailScreen> {
   }
 
   deleteSubmit() async {
-
-    final action = await Dialogs.yesAbortDialog(
-        context, 'Confirm to delete?',
-        'Once confirmed, it cannot be modified anymore',
-        'Delete');
+    final action = await Dialogs.yesAbortDialog(context, 'Confirm to delete?',
+        'Once confirmed, it cannot be modified anymore', 'Delete');
 
     if (action == DialogAction.yes) {
       String res = await FireStoreMethods().deleteCar(widget.snap["carId"]);
@@ -63,7 +60,7 @@ class _CarDetailScreenState extends State<CarDetailScreen> {
           isLoading = false;
         });
         Navigator.of(context).pop();
-        showSnackBar(context, "Car removed successfully");
+        showSnackBar(context, "Car removed successfully  ");
       } else {
         setState(() {
           isLoading = false;
@@ -71,191 +68,200 @@ class _CarDetailScreenState extends State<CarDetailScreen> {
         showSnackBar(context, res);
       }
     }
-
-
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     return isLoading
-        ? const LoadingIndicator() : Scaffold(
-      appBar: PrimaryAppBar(
-          title: "Car Details"
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.background,
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                // margin: EdgeInsets.symmetric(horizontal: 25.0),
-                decoration: BoxDecoration(
-                  // color: Theme.of(context).colorScheme.secondaryContainer,
-                ),
-                child: GestureDetector(
-                  child: Hero(
-                    tag: 'imageHero',
-                    child: Image(
-                      height: 200,
-                      width: double.infinity,
-                      // width: double.infinity - 20,
-                      image: NetworkImage( widget.snap["photoUrl"]),
-                      loadingBuilder: (BuildContext context, Widget child,
-                          ImageChunkEvent? loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return Center(
-                          child: CircularProgressIndicator(
-                            value: loadingProgress.expectedTotalBytes != null
-                                ? loadingProgress.cumulativeBytesLoaded /
-                                loadingProgress.expectedTotalBytes!
-                                : null,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (_) {
-                      return ImageFullScreen(imageUrl: widget.snap["photoUrl"],);
-                    }));
-                  },
-                ),
+        ? const LoadingIndicator()
+        : Scaffold(
+            appBar: PrimaryAppBar(title: "Car Details"),
+            body: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.background,
               ),
-
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 35, vertical: 25),
-                decoration: BoxDecoration(
-                  color: Color(0xFFF1F8FF),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
+              child: SingleChildScrollView(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      widget.snap["carName"],
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-
-                    const SizedBox(height: 20,),
-
-                    Row(
-                      children: [
-                        Expanded(
-                          child: subtitleText("Year"),
-                        ),
-                        Expanded(
-                          child: subtitleText("Brand"),
-                        ),
-
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: contentText(widget.snap["year"].toString()),
-                        ),
-                        Expanded(
-                          child: contentText(widget.snap["brand"]),
-                        ),
-
-                      ],
-                    ),
-
-                    const SizedBox(height: 20,),
-                    subtitleText("Model"),
-                    contentText(widget.snap["model"]),
-                    const SizedBox(height: 20,),
-                    subtitleText("Seats"),
-                    contentText(widget.snap["seat"].toString()),
-                    const SizedBox(height: 20,),
-                    subtitleText("Transmission"),
-                    contentText(widget.snap["transmission"]),
-                    const SizedBox(height: 20,),
-
-                    FirebaseAuth.instance.currentUser!.uid == widget.snap["ownerId"] ?
                     Container(
-                      child: PrimaryButton(
-                        onPressed: deleteSubmit,
-                        childText: "Delete",
-                      ),
-                    )
-                    :
-                    Container(
+                      // margin: EdgeInsets.symmetric(horizontal: 25.0),
                       decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 30,),
-                            child: Row(
-                              children: [
-                                Text(
-                                  "RM ${widget.snap["pricePerDay"].toStringAsFixed(2)} ",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                Text(
-                                  "/ Day",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                  ),
-                                ),
-                              ],
-                            ),
+                          // color: Theme.of(context).colorScheme.secondaryContainer,
                           ),
-
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (_) {
-                                return RentNowScreen(snap: widget.snap,);
-                              }));
+                      child: GestureDetector(
+                        child: Hero(
+                          tag: 'imageHero',
+                          child: Image(
+                            height: 200,
+                            width: double.infinity,
+                            // width: double.infinity - 20,
+                            image: NetworkImage(widget.snap["photoUrl"]),
+                            loadingBuilder: (BuildContext context, Widget child,
+                                ImageChunkEvent? loadingProgress) {
+                              if (loadingProgress == null) return child;
+                              return Center(
+                                child: CircularProgressIndicator(
+                                  value: loadingProgress.expectedTotalBytes !=
+                                          null
+                                      ? loadingProgress.cumulativeBytesLoaded /
+                                          loadingProgress.expectedTotalBytes!
+                                      : null,
+                                ),
+                              );
                             },
-                            child: Container(
-                              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFEBE3F),
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                              child: Text(
-                                "Rent Now",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                ),
-                              ),
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_) {
+                            return ImageFullScreen(
+                              imageUrl: widget.snap["photoUrl"],
+                            );
+                          }));
+                        },
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 35, vertical: 25),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF1F8FF),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.snap["carName"],
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: subtitleText("Year"),
+                              ),
+                              Expanded(
+                                child: subtitleText("Brand"),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child:
+                                    contentText(widget.snap["year"].toString()),
+                              ),
+                              Expanded(
+                                child: contentText(widget.snap["brand"]),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          subtitleText("Model"),
+                          contentText(widget.snap["model"]),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          subtitleText("Seats"),
+                          contentText(widget.snap["seat"].toString()),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          subtitleText("Transmission"),
+                          contentText(widget.snap["transmission"]),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          FirebaseAuth.instance.currentUser!.uid ==
+                                  widget.snap["ownerId"]
+                              ? Container(
+                                  child: PrimaryButton(
+                                    onPressed: deleteSubmit,
+                                    childText: "Delete",
+                                  ),
+                                )
+                              : Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.symmetric(
+                                          horizontal: 30,
+                                        ),
+                                        child: Row(
+                                          children: [
+                                            Text(
+                                              "RM ${widget.snap["pricePerDay"].toStringAsFixed(2)} ",
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                            Text(
+                                              "/ Day",
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {
+                                          Navigator.push(context,
+                                              MaterialPageRoute(builder: (_) {
+                                            return RentNowScreen(
+                                              snap: widget.snap,
+                                            );
+                                          }));
+                                        },
+                                        child: Container(
+                                          padding: EdgeInsets.symmetric(
+                                              horizontal: 20, vertical: 15),
+                                          decoration: BoxDecoration(
+                                            color: Color(0xFFFEBE3F),
+                                            borderRadius:
+                                                BorderRadius.circular(30),
+                                          ),
+                                          child: Text(
+                                            "Rent Now",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                         ],
                       ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
-
 }
